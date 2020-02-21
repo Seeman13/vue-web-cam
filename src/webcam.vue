c71661578223fb69fbd8510592e44d2bda43654b
Александр Королёв <forzaazzuri13@gmail.com>
2020-02-21 12:53:45 +0300

    audio capture
    In component simply added new prop audio with help him will possible primitive capture audio.

--- src/webcam.vue
+++ src/webcam.vue
@@ -11,12 +11,12 @@
 
 <script>
 export default {
-  name: "VueWebCam",
+  name: 'VueWebCam',
 
   props: {
     width: {
       type: [Number, String],
-      default: "100%"
+      default: '100%'
     },
     height: {
       type: [Number, String],
@@ -28,7 +28,7 @@ export default {
     },
     screenshotFormat: {
       type: String,
-      default: "image/jpeg"
+      default: 'image/jpeg'
     },
     selectFirstDevice: {
       type: Boolean,
@@ -46,246 +46,250 @@ export default {
       type: Object,
       default: null,
       validator: value => {
-        return value.height && value.width;
+        return value.height && value.width
       }
+    },
+    audio: {
+      type: Boolean,
+      default: false
     }
   },
 
-  data() {
+  data () {
     return {
       source: null,
       canvas: null,
       camerasListEmitted: false,
       cameras: []
-    };
-  },
-
-  watch: {
-    deviceId: function(id) {
-      this.changeCamera(id);
     }
   },
 
-  mounted() {
-    this.setupMedia();
+  watch: {
+    deviceId: function (id) {
+      this.changeCamera(id)
+    }
   },
 
-  beforeDestroy() {
-    this.stop();
+  mounted () {
+    this.setupMedia()
+  },
+
+  beforeDestroy () {
+    this.stop()
   },
 
   methods: {
     /**
-     * get user media
-     */
-    legacyGetUserMediaSupport() {
+       * get user media
+       */
+    legacyGetUserMediaSupport () {
       return constraints => {
         // First get ahold of the legacy getUserMedia, if present
         let getUserMedia =
-          navigator.getUserMedia ||
-          navigator.webkitGetUserMedia ||
-          navigator.mozGetUserMedia ||
-          navigator.msGetUserMedia ||
-          navigator.oGetUserMedia;
+            navigator.getUserMedia ||
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+            navigator.msGetUserMedia ||
+            navigator.oGetUserMedia
 
         // Some browsers just don't implement it - return a rejected promise with an error
         // to keep a consistent interface
         if (!getUserMedia) {
           return Promise.reject(
-            new Error("getUserMedia is not implemented in this browser")
-          );
+            new Error('getUserMedia is not implemented in this browser')
+          )
         }
 
         // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
-        return new Promise(function(resolve, reject) {
-          getUserMedia.call(navigator, constraints, resolve, reject);
-        });
-      };
+        return new Promise(function (resolve, reject) {
+          getUserMedia.call(navigator, constraints, resolve, reject)
+        })
+      }
     },
 
     /**
-     * setup media
-     */
-    setupMedia() {
+       * setup media
+       */
+    setupMedia () {
       if (navigator.mediaDevices === undefined) {
-        navigator.mediaDevices = {};
+        navigator.mediaDevices = {}
       }
 
       if (navigator.mediaDevices.getUserMedia === undefined) {
-        navigator.mediaDevices.getUserMedia = this.legacyGetUserMediaSupport();
+        navigator.mediaDevices.getUserMedia = this.legacyGetUserMediaSupport()
       }
 
-      this.testMediaAccess();
+      this.testMediaAccess()
     },
 
     /**
-     * load available cameras
-     */
-    loadCameras() {
+       * load available cameras
+       */
+    loadCameras () {
       navigator.mediaDevices
         .enumerateDevices()
         .then(deviceInfos => {
           for (let i = 0; i !== deviceInfos.length; ++i) {
-            let deviceInfo = deviceInfos[i];
-            if (deviceInfo.kind === "videoinput") {
-              this.cameras.push(deviceInfo);
+            let deviceInfo = deviceInfos[i]
+            if (deviceInfo.kind === 'videoinput') {
+              this.cameras.push(deviceInfo)
             }
           }
         })
         .then(() => {
           if (!this.camerasListEmitted) {
             if (this.selectFirstDevice && this.cameras.length > 0) {
-              this.deviceId = this.cameras[0].deviceId;
+              this.deviceId = this.cameras[0].deviceId
             }
 
-            this.$emit("cameras", this.cameras);
-            this.camerasListEmitted = true;
+            this.$emit('cameras', this.cameras)
+            this.camerasListEmitted = true
           }
         })
-        .catch(error => this.$emit("notsupported", error));
+        .catch(error => this.$emit('notsupported', error))
     },
 
     /**
-     * change to a different camera stream, like front and back camera on phones
-     */
-    changeCamera(deviceId) {
-      this.stop();
-      this.$emit("camera-change", deviceId);
-      this.loadCamera(deviceId);
+       * change to a different camera stream, like front and back camera on phones
+       */
+    changeCamera (deviceId) {
+      this.stop()
+      this.$emit('camera-change', deviceId)
+      this.loadCamera(deviceId)
     },
 
     /**
-     * load the stream to the
-     */
-    loadSrcStream(stream) {
-      if ("srcObject" in this.$refs.video) {
+       * load the stream to the
+       */
+    loadSrcStream (stream) {
+      if ('srcObject' in this.$refs.video) {
         // new browsers api
-        this.$refs.video.srcObject = stream;
+        this.$refs.video.srcObject = stream
       } else {
         // old broswers
-        this.source = window.HTMLMediaElement.srcObject(stream);
+        this.source = window.HTMLMediaElement.srcObject(stream)
       }
       // Emit video start/live event
       this.$refs.video.onloadedmetadata = () => {
-        this.$emit("video-live", stream);
-      };
+        this.$emit('video-live', stream)
+      }
 
-      this.$emit("started", stream);
+      this.$emit('started', stream)
     },
 
     /**
-     * stop the selected streamed video to change camera
-     */
-    stopStreamedVideo(videoElem) {
-      let stream = videoElem.srcObject;
-      let tracks = stream.getTracks();
+       * stop the selected streamed video to change camera
+       */
+    stopStreamedVideo (videoElem) {
+      let stream = videoElem.srcObject
+      let tracks = stream.getTracks()
 
       tracks.forEach(track => {
         // stops the video track
-        track.stop();
-        this.$emit("stopped", stream);
+        track.stop()
+        this.$emit('stopped', stream)
 
-        this.$refs.video.srcObject = null;
-        this.source = null;
-      });
+        this.$refs.video.srcObject = null
+        this.source = null
+      })
     },
 
     // stop the video
-    stop() {
+    stop () {
       if (this.$refs.video !== null && this.$refs.video.srcObject) {
-        this.stopStreamedVideo(this.$refs.video);
+        this.stopStreamedVideo(this.$refs.video)
       }
     },
 
     // start the video
-    start() {
+    start () {
       if (this.deviceId) {
-        this.loadCamera(this.deviceId);
+        this.loadCamera(this.deviceId)
       }
     },
 
     // pause the video
-    pause() {
+    pause () {
       if (this.$refs.video !== null && this.$refs.video.srcObject) {
-        this.$refs.video.pause();
+        this.$refs.video.pause()
       }
     },
 
     // resume the video
-    resume() {
+    resume () {
       if (this.$refs.video !== null && this.$refs.video.srcObject) {
-        this.$refs.video.play();
+        this.$refs.video.play()
       }
     },
 
     /**
-     * test access
-     */
-    testMediaAccess() {
-      let constraints = { video: true };
+       * test access
+       */
+    testMediaAccess () {
+      let constraints = { video: true }
 
       if (this.resolution) {
-        constraints.video = {};
-        constraints.video.height = this.resolution.height;
-        constraints.video.width = this.resolution.width;
+        constraints.video = {}
+        constraints.video.height = this.resolution.height
+        constraints.video.width = this.resolution.width
       }
 
       navigator.mediaDevices
         .getUserMedia(constraints)
         .then(stream => {
-          //Make sure to stop this MediaStream
-          let tracks = stream.getTracks();
+          // Make sure to stop this MediaStream
+          let tracks = stream.getTracks()
           tracks.forEach(track => {
-            track.stop();
-          });
-          this.loadCameras();
+            track.stop()
+          })
+          this.loadCameras()
         })
-        .catch(error => this.$emit("error", error));
+        .catch(error => this.$emit('error', error))
     },
 
     /**
-     * load the camera passed as index!
-     */
-    loadCamera(device) {
-      let constraints = { video: { deviceId: { exact: device } } };
+       * load the camera passed as index!
+       */
+    loadCamera (device) {
+      let constraints = { video: { deviceId: { exact: device } }, audio: this.audio }
 
       if (this.resolution) {
-        constraints.video.height = this.resolution.height;
-        constraints.video.width = this.resolution.width;
+        constraints.video.height = this.resolution.height
+        constraints.video.width = this.resolution.width
       }
 
       navigator.mediaDevices
         .getUserMedia(constraints)
         .then(stream => this.loadSrcStream(stream))
-        .catch(error => this.$emit("error", error));
+        .catch(error => this.$emit('error', error))
     },
 
     /**
-     * capture screenshot
-     */
-    capture() {
-      return this.getCanvas().toDataURL(this.screenshotFormat);
+       * capture screenshot
+       */
+    capture () {
+      return this.getCanvas().toDataURL(this.screenshotFormat)
     },
 
     /**
-     * get canvas
-     */
-    getCanvas() {
-      let video = this.$refs.video;
+       * get canvas
+       */
+    getCanvas () {
+      let video = this.$refs.video
       if (!this.ctx) {
-        let canvas = document.createElement("canvas");
-        canvas.height = video.videoHeight;
-        canvas.width = video.videoWidth;
-        this.canvas = canvas;
+        let canvas = document.createElement('canvas')
+        canvas.height = video.videoHeight
+        canvas.width = video.videoWidth
+        this.canvas = canvas
 
-        this.ctx = canvas.getContext("2d");
+        this.ctx = canvas.getContext('2d')
       }
 
-      const { ctx, canvas } = this;
-      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      const { ctx, canvas } = this
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
 
-      return canvas;
+      return canvas
     }
   }
-};
+}
 </script>
